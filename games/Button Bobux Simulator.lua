c5953c70e21027aee649cff8eef59b7240d0a195
stasiek <the.orginal.stasgaming@gmail.com>
2023-02-04 23:59:25 +0100

    Create Button Bobux Simulator.lua

---
+++ games/Button Bobux Simulator.lua
@@ -0,0 +1,27 @@
+tps = {
+	Fans = Vector3.new(74, 22, 202),
+	Games = Vector3.new(74, 22, 172),
+	Groups = Vector3.new(74, 22, 142),
+	StarCreators = Vector3.new(74, 22, 112),
+	BigGames = Vector3.new(74, 22, 82),
+	BigGroups = Vector3.new(74, 22, 52),
+	Developers = Vector3.new(74, 22, 22),
+	MinesSpawn = Vector3.new(9520, 370, -101),
+	DungeonsSpawn = Vector3.new(-911, 50, -66391)
+}
+
+GamesTab:AddLabel("Button Bobux Simulator")
+
+local l = {}
+
+for key, value in pairs(tps) do
+    table.insert(l, key)
+end
+
+GamesTab:AddDropdown({
+	Name = "Teleports",
+	Options = l,
+	Callback = function(Value)
+		a.Character.HumanoidRootPart.CFrame = CFrame.new(tps[Value])
+	end
+})
